e5da8f8b50913ec78f5458d2a8aa42fbe63dbfd9
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-15 17:15:18 +0200

    Update the script

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -3,6 +3,7 @@
 Extend your Python script to export data in the JSON format.
 Records all tasks from all employees
 """
+import json
 import requests
 from sys import argv
 
@@ -24,21 +25,4 @@ if __name__ == "__main__":
                              for task in todos]
 
     with open("todo_all_employees.json", "w") as json_file:
-        json_file.write("{")
-        for user_id, tasks in all_data.items():
-            json_file.write("\"{}\": ".format(user_id))
-            json_file.write("[")
-            for task in tasks:
-                json_file.write("{")
-                json_file.write("\"task\": \"{}\", ".format(task.get("task")))
-                json_file.write("\"completed\": \"{}\", ".format(
-                    task.get("completed")))
-                json_file.write("\"username\": \"{}\"".format(
-                    task.get("username")))
-                json_file.write("}")
-                if task != tasks[-1]:
-                    json_file.write(", ")
-            json_file.write("]")
-            if user_id != list(all_data.keys())[-1]:
-                json_file.write(", ")
-        json_file.write("}")
+        json_file.write(json.dumps(all_data))
